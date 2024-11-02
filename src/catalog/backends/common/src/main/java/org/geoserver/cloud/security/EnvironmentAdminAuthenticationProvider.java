3c2f1982d4e9da2d331bab03605a61e77c6f5d28
Michael Scott <camrymps@gmail.com>
2024-11-01 21:03:52 -0400

    test #16.

--- src/catalog/backends/common/src/main/java/org/geoserver/cloud/security/EnvironmentAdminAuthenticationProvider.java
+++ src/catalog/backends/common/src/main/java/org/geoserver/cloud/security/EnvironmentAdminAuthenticationProvider.java
@@ -25,7 +25,7 @@ import org.springframework.util.StringUtils;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 /**
  * {@link AuthenticationProvider} that allows to set an administrator account (username and
