a2c23c00d953dfef5ac53ba2d5e4c2623718c4a2
Michael Scott <camrymps@gmail.com>
2024-11-02 09:55:31 -0400

    test #24.

--- src/starters/webmvc/src/main/java/org/geoserver/cloud/autoconfigure/context/GeoServerContextInitializer.java
+++ src/starters/webmvc/src/main/java/org/geoserver/cloud/autoconfigure/context/GeoServerContextInitializer.java
@@ -13,9 +13,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-import jakarta.servlet.Filter; // Added
+import jakarta.servlet.Filter;
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletContextListener; // Added
+import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.ServletContextEvent;
 
 /**
